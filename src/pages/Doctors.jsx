9572e3589f61e0a23c3c7f4b0f8114d017fa032e
prem-code123 <sarkarprem2003@gmail.com>
2025-07-06 16:48:21 +0530

    doctimage

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -1,41 +1,46 @@
 import React from 'react';
 import DoctorCard from '../component/Doctor-card';
 import '../component/Card-Container.css';
+import Rahul from '../assets/doctimage/doct1.jpeg';
+import Aditi from '../assets/doctimage/doct2.jpeg';
+import Priya from '../assets/doctimage/doct3.jpeg';
+import Neha from '../assets/doctimage/doct4.jpeg';
+import Sameer from '../assets/doctimage/doct5.jpeg';
 
 const doctors = [
   {
     id: 1,
     name: 'Dr. Aditi Sharma',
     desc: 'General Physician, 10+ years experience',
-    img: 'https://images.unsplash.com/photo-1511174511562-5f97f4f4eab6?auto=format&fit=crop&w=400&q=80',
+    img: Aditi,
     fee: '₹400'
   },
   {
     id: 2,
     name: 'Dr. Rahul Mehra',
     desc: 'Cardiologist, 15+ years experience',
-    img: 'https://images.unsplash.com/photo-1527613426441-4da17471b66d?auto=format&fit=crop&w=400&q=80',
+    img: Rahul,
     fee: '₹700'
   },
   {
     id: 3,
     name: 'Dr. Priya Verma',
     desc: 'Dermatologist, 8+ years experience',
-    img: 'https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?auto=format&fit=crop&w=400&q=80',
+    img: Priya,
     fee: '₹500'
   },
   {
     id: 4,
     name: 'Dr. Sameer Khan',
     desc: 'Pediatrician, 12+ years experience',
-    img: 'https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?auto=format&fit=crop&w=400&q=80',
+    img: Sameer,
     fee: '₹450'
   },
   {
     id: 5,
     name: 'Dr. Neha Singh',
     desc: 'Gynecologist, 9+ years experience',
-    img: 'https://images.unsplash.com/photo-1517841905240-472988babdf9?auto=format&fit=crop&w=400&q=80',
+    img: Neha ,
     fee: '₹600'
   },
 ];
